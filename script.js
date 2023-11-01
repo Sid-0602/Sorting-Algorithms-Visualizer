11000ea16a1132e8725eb5c46c93f303264bc7ea
Siddhant J <sidbnjadhav@gmail.com>
2023-11-01 18:40:27 +0530

    bubble Sort algo code push

--- script.js
+++ script.js
@@ -57,23 +57,6 @@ function animate(moves){
     },50);
 }
 
-function bubbleSort(array){
-    const moves=[];
-    do{
-        var swapped = false;
-        for(let i=1;i<array.length;i++){
-            moves.push({ indices:[i-1,i],type:"comparison"});
-            if(array[i-1]>array[i]){
-                swapped=true;
-                moves.push({ indices:[i-1,i],type:"swap"});
-                [array[i-1],array[i]] = [array[i],array[i-1]];
-            }
-        }
-    }while(swapped){
-        return moves;
-    }
-    
-}
 
 
 function showBars(move){
---
+++ sorting_algo/bubbleSort.js
@@ -0,0 +1,17 @@
+function bubbleSort(array){
+    const moves=[];
+    do{
+        var swapped = false;
+        for(let i=1;i<array.length;i++){
+            moves.push({ indices:[i-1,i],type:"comparison"});
+            if(array[i-1]>array[i]){
+                swapped=true;
+                moves.push({ indices:[i-1,i],type:"swap"});
+                [array[i-1],array[i]] = [array[i],array[i-1]];
+            }
+        }
+    }while(swapped){
+        return moves;
+    }
+    
+}
